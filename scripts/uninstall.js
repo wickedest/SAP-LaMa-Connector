0871ae47f0714c7caf96840cff4c033d39308aa3
Jamie Peabody <jamie.peabody@gmail.com>
2019-12-11 16:31:56 +0000

    Updated to use fs and INIT_CWD

--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -1,13 +1,5 @@
-'use strict'
+const path = require('path');
+const fs = require('fs');
 
-var shell = require('shelljs');
-
-var filesToRemove = ['./swagger/sap-lama-api.json', './swagger/sap-lama-api.png']
-
-var swaggerDir = process.env.INIT_CWD + '/swagger'
-
-// shell.set('-v');
-
-//shell.cd(swaggerDir);
-
-shell.rm(filesToRemove);
+fs.unlinkSync(path.join(process.env.INIT_CWD, 'swagger', 'sap-lama-api.json'));
+fs.unlinkSync(path.join(process.env.INIT_CWD, 'swagger', 'sap-lama-api.png'));
