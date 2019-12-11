e21c91d7fd19a60082a625b66559ec975933d81a
Jamie Peabody <jamie.peabody@gmail.com>
2019-12-11 16:32:58 +0000

    Update to not use shell dependency

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,11 +1,5 @@
-'use strict'
-
-var shell = require('shelljs');
-
-var filesToCopy = ['./swagger/sap-lama-api.json', './swagger/sap-lama-api.png']
-
-var swaggerDir = process.env.INIT_CWD + '/swagger'
-
-// shell.set('-v');
-
-shell.cp(filesToCopy, swaggerDir);
+const fs = require('fs');
+const path = require('path');
+const dest = path.join(process.env.INIT_CWD, 'swagger');
+fs.copyFileSync(path.join(__dirname, '..', 'swagger', 'sap-lama-api.json'), dest);
+fs.copyFileSync(path.join(__dirname, '..', 'swagger', 'sap-lama-api.png'), dest);
